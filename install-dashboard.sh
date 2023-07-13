1a03f9c5f400217db21945e251fae2ac63c6a0d8
Nguyễn Trung Nghĩa <nghianguyen@Nguyens-MacBook-Pro.local>
2023-07-13 10:16:53 +0700

    update readme

--- install-dashboard.sh
+++ install-dashboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Deployment dashboard-v2.7.0"
-kubectl apply -f install-dashboard.sh
+kubectl apply -f dashboard-v2.7.0.yaml
 sleep 10
 
 echo "Create kubernetes-dashboard-certs, SSL using Openssl"
@@ -9,15 +9,17 @@ sudo chmod 777 -R certs
 openssl req -nodes -newkey rsa:2048 -keyout certs/dashboard.key -out certs/dashboard.csr -subj "/C=/ST=/L=/O=/OU=/CN=kubernetes-dashboard"
 openssl x509 -req -sha256 -days 365 -in certs/dashboard.csr -signkey certs/dashboard.key -out certs/dashboard.crt
 sudo chmod -R 777 certs
+kubectl create secret generic kubernetes-dashboard-certs --from-file=certs -n kubernetes-dashboard
+sleep 5
 
 #Install dashboard
 echo "Installing dashboard"
 kubectl apply -f admin-user.yaml
-sleep 10s
+sleep 5s
 
 #Install metric
 echo "Installing metric"
-kubeclt apply -f metric-install.yaml
+kubectl apply -f metric-install.yaml
 sleep 5s
 echo "Dashboard K8S install successfully"
 
